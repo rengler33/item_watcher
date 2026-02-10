6aeddd7c8b9610db05b8b06ac6996e6a54193cde
Robert Engler <rengler33@gmail.com>
2026-02-09 22:46:19 -0600

    Add dynamic supervisor

--- item_watcher/lib/item_watcher/application.ex
+++ item_watcher/lib/item_watcher/application.ex
@@ -1,6 +1,4 @@
 defmodule ItemWatcher.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,9 @@ defmodule ItemWatcher.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: ItemWatcher.Worker.start_link(arg)
-      # {ItemWatcher.Worker, arg}
+      {DynamicSupervisor, name: ItemWatcher.ItemSupervisor, strategy: :one_for_one}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ItemWatcher.Supervisor]
     Supervisor.start_link(children, opts)
   end
